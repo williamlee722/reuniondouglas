52ca7f6951852ae7d3a3cc0e967baf021bd6e622
williamlee722 <99303686+williamlee722@users.noreply.github.com>
2022-12-06 03:28:33 -0800

    db model added

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SigninPage from './pages/SigninPage';
+import axios from "axios";
 
-var flag = 0;
 
 function App() {
 
-  const [apiData,setApiData] = useState({});
+  // Api
+  const [apiData,setApiData] = useState({}); 
 
   useEffect(() => {
     fetch("/api").then(
@@ -19,15 +20,65 @@ function App() {
       console.log(apiData);
     }
     )
-  },[flag])
+  },[])
+
+  // MongoDB
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const url = "http://localhost:5000/api/bloginfo";
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res, res.data);
+        setBlogs(res.data);
+      })
+      .catch((err) => {
+        console.log("ERROR: ", err);
+      });
+  }, []);
+
+  //Delete the blogs from the database
+  const handleDelete = async (blog) => {
+    console.log("delete handled");
+    const url = `http://localhost:5000/api/bloginfo/${blog._id}`;
+    const { data } = await axios.delete(url);
+    console.log(data);
+    const newBlogs = blogs.filter((b) => b._id !== blog._id);
+    setBlogs(newBlogs);
+  };
+
+  //Adding the new blog
+  const handleAdd = async (title, author, headerImage, text) => {
+    console.log("Add blog handled");
+    console.log(title, author, headerImage, text)
+    const url = `http://localhost:5000/api/bloginfo`;
+    const { data } = await axios.post(url, {
+      title, 
+      author, 
+      headerImage, 
+      text
+    });
+    console.log(data);
+    if (typeof data == "object") {
+      const newBlogs = [...blogs, data];
+      setBlogs(newBlogs);
+    } else {
+      console.log("Add couldn't be completed.");
+    }
+  };
+
+  const handleUpdate=()=>{
+    console.log("update")
+  }
 
   return (
     <Router>
       <Routes>
         <Route path='/' element={
-        <Home onApiData= {apiData}/>
+        <Home onApiData= {apiData} blogs={blogs}/>
         }/>
-        <Route path='/admin' element={<SigninPage />}  />
+        <Route path='/admin' element={<SigninPage onAdd={handleAdd} onDelete={handleDelete} onUpdate={handleUpdate} blogs={blogs}/>}  />
       </Routes>
     </Router>
   );
--- Server/server.js
+++ Server/server.js
@@ -3,16 +3,24 @@
 const express = require("express")
 const app = express();
 
+const mongoose = require("mongoose");
+const Blog = require("./models/blogs.js");
+const cors = require("cors");
+
 const bodyParser = require("body-parser")
 
 const https = require("https")
 const axios = require("axios")
 
+app.use(cors())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"));
 
 
 const apiKey = "5be4a60131798e59349d3764e174a738";
 const units = "metric";
+
+const dbUrl = "mongodb://localhost:27017/blogsDB";
 
 // var count = 0;
 
@@ -42,28 +50,6 @@ app.get("/api", (req,res) => {
         .catch((err) => {
             console.log("ERROR: ", err);
     });
-
-
-    // https.get(url,(response) =>{
-    //     console.log(response.statusCode); 
-    //     // count++
-    //     // console.log(count); 
-    //     response.on("data",(data) => {
-    //         //console.log(data);
-    //         const weatherData = JSON.parse(data);
-    //         console.log(weatherData);
-
-    //         const description = weatherData.weather[0].description;
-    //         const temp = Math.round(weatherData.main.temp);
-    //         const country = weatherData.sys.country;
-
-    //         // https://openweathermap.org/weather-conditions
-    //         const icon = weatherData.weather[0].icon;
-    //         const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-
-    //         res.json({"city": city, "country": country, "temp": temp, "iconImageURL": imageURL, "description": description})
-    //     })}        
-    // )
     
 
     // https.get('https://api.open-meteo.com/v1/forecast?latitude=49.25&longitude=-123.12&current_weather=true&timezone=Canada/Pacific',(response) => {
@@ -74,12 +60,130 @@ app.get("/api", (req,res) => {
     
     //             const temp = Math.round(weatherData.current_weather.temperature);
     
-    //             res.json({"city": "Vancouver", "country": "Canada", "temp": temp, "iconImageURL": "No icon", "description": "No description"});
-    //             })})
-    
-    
+    //             res.json({"city": "Vancouver", "country": "Canada", "temp": temp, "iconImageURL": "", "description": "No description"});
+    //             })})       
 })
+
+
+//READ OPERATION
+
+app.get("/api/bloginfo", async (req, res) => {
+    try {
+      await mongoose.connect(dbUrl);
+      console.log("database connected");
+      Blog.find((err, blogs) => {
+        if (err) res.send(err);
+        else {
+          console.log(blogs);
+          res.send(blogs);
+          mongoose.connection.close();
+        }
+      });
+    } catch (err) {
+      console.log(`ERROR: ${err}`);
+    }
+  });
+  
+ //CREATE OPERATION
+app.post("/api/bloginfo", async (req, res) => {
+    try {
+      //process request body
+      const { title, author, headerImage, text } = req.body;
+      console.log(title, author, headerImage, text);
+  
+      const blog = new Blog({
+        title, author, headerImage, text
+      });
+      await mongoose.connect(dbUrl);
+      blog.save((err) => {
+        if (err) {
+          console.log("ERROR: ", err);
+          res.send(err);
+        } else {
+          console.log("Document inserted successfully");
+          res.send(blog);
+          mongoose.connection.close();
+        }
+      });
+    } catch (err) {
+      console.log(`ERROR: ${err}`);
+    }
+});
+  
+  ///////////UPDATE OPERATION
+  
+  app.put("/api/bloginfo/:id", async (req, res) => {
+    console.log("inside update");
+    try {
+      //process request body
+      const { title, author, headerImage, text } = req.body;
+      console.log(title, author, headerImage, text);
+  
+      let _id = req.params.id;
+      _id = mongoose.Types.ObjectId(_id);
+  
+      await mongoose.connect(dbUrl);
+      console.log("database connected");
+      Blog.updateOne(
+        { _id: _id },
+        { title, author, headerImage, text},
+  
+        (err, msg) => {
+          if (err) {
+            console.log("ERROR: ", err);
+            res.send(err);
+          } else {
+            if (msg.modifiedCount == 0) {
+              console.log(`No matching blog found`);
+              res.send(`No matching blog found`);
+            } else {
+              console.log(`Successfully updated ${msg.modifiedCount} blog`);
+              res.send(`Successfully updated ${msg.modifiedCount} blog`);
+            }
+            mongoose.connection.close();
+          }
+        }
+      );
+    } catch (err) {
+      console.log(`ERROR: ${err}`);
+    }
+  });
+  
+  ///////////DELETE OPERATION
+  
+  app.delete("/api/bloginfo/:id", async (req, res) => {
+    console.log("inside delete");
+    try {
+      let _id = req.params.id;
+      _id = mongoose.Types.ObjectId(_id);
+  
+      await mongoose.connect(dbUrl);
+      console.log("database connected");
+      Blog.deleteOne(
+        { _id: _id },
+  
+        (err, msg) => {
+          if (err) {
+            console.log("ERROR: ", err);
+            res.send(err);
+          } else {
+            if (msg.deletedCount == 0) {
+              console.log(`No matching blog found`);
+              res.send(`No matching blog found`);
+            } else {
+              console.log(`Successfully deleted ${msg.deletedCount} blog`);
+              res.send(`Successfully deleted ${msg.deletedCount} blog`);
+            }
+            mongoose.connection.close();
+          }
+        }
+      );
+    } catch (err) {
+      console.log(`ERROR: ${err}`);
+    }
+  });
 
 app.listen(5000, () => {
     console.log("The serves is up and running on port 5000");
 })
+
