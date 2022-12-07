554fb8f55fc0ce5df7f26fb76f8db8d4d9682c7e
williamlee722 <99303686+williamlee722@users.noreply.github.com>
2022-12-07 00:22:51 -0800

    all bugs fixed, last commit, William out

--- Client/src/components/WeatherApi.jsx
+++ Client/src/components/WeatherApi.jsx
@@ -10,19 +10,19 @@ const WeatherApi = ({apiData}) => {
     console.log(iconImageURL);
 
     return ( 
-        <div style={{position: 'fixed', bottom: '0', right: '0'}}>
-            <table className='weatherApiBox' style={{width: '90px', height: '50px', padding: '5px', borderRadius: '10px', margin: '5px'}}>
+        <div className='weatherApi'>
+            <table className='weatherApiBox'>
                 <tbody>
-                    <tr style={{height: '20px'}}>
+                    <tr className='weatherIcon'>
                         <td rowSpan='2'> 
                             <img src={iconImageURL} alt='WeatherIcon' style={{width: '50px',  height: '50px'}}></img>
                         </td>
-                        <td style={{fontSize: '14px'}}>
+                        <td className='weatherApicity'>
                             {city}
                         </td>
                     </tr>
-                    <tr style={{height: '40px'}}>
-                        <td style={{fontSize: '18px'}}>
+                    <tr className='weatherdegrees'>
+                        <td className='weatherApitemp'>
                             {temp} &#8451;
                         </td>
                     </tr>
